865a8fcf71ce1ce34d6eead90c0dc340b85e2847
pollybrandao <pollyannybrandao@gmail.com>
2025-06-22 23:01:00 -0300

    Adição de novos 3 testes: Marcação e desmarcação de tarefas; Limpeza de tarefas com o btn Clear Completed; Atualização do contator de tarefas;

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -69,3 +69,60 @@ describe('TODOMvc App', () => {
       .should('have.length', 2);
   });
 });
+
+it('Marca e desmarca uma tarefa como concluída', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar Cypress{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.class', 'completed');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .should('not.have.class', 'completed');
+  });
+
+it('Limpa tarefas concluídas usando "Clear Completed"', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Atividade 1{enter}')
+      .type('Atividade 2{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('.clear-completed')
+      .should('be.visible') 
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Atividade 2');
+  });
+
+it('Atualiza corretamente o contador de tarefas ativas', () => {
+  cy.visit('');
+
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 1{enter}')
+    .type('Tarefa 2{enter}')
+    .type('Tarefa 3{enter}');
+
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .first()
+    .click();
+
+  cy.get('.todo-count')
+    .should('have.text', '2 items left');
+});
