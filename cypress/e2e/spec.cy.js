30b9a7cd52367c011c1f53c1dc5a7fcea5bddb7f
davi <davi.araujo@onfly.com.br>
2025-06-23 08:35:16 -0300

    add mew e2e tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,57 @@
 describe('TODOMvc App', () => {
+
+  it('Edits an existing todo item', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('Original Task{enter}');
+
+    cy.get('[data-cy=todos-list] li label')
+        .dblclick();
+
+    const new_text =  Math.random().toString()
+    cy.get('[data-cy=todos-list] li .edit')
+        .clear()
+        .type(new_text + '{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+        .should('have.text', new_text);
+  });
+
+  it('active filter show only not completed', () => {
+    cy.visit('');
+
+    for (let i = 0; i < 10; i++) {
+      cy.get('[data-cy=todo-input]')
+          .type(`Task ${i}{enter}`);
+    }
+
+    for (let i = 0; i < 6; i++) {
+      cy.get('[data-cy=todos-list] li [data-cy=toggle-todo-checkbox]')
+          .eq(i)
+          .click();
+    }
+
+    cy.get('[data-cy=filter-active-link]')
+        .click();
+
+    cy.get('[data-cy=todos-list] li')
+        .should('have.length', 4) // 10 - 6
+        .each(($el, index) => {
+          cy.wrap($el).should('contain', `Task ${index + 6}`);
+        });
+  });
+
+  it('Does not add empty todo items', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+        .type('{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+        .should('have.length', 0);
+  });
+
   it('Verifica se app está abrindo', () => {
     cy.visit('')
   })
